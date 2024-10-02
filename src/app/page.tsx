27a4f6f2bb99d65cb9fedc7f74a3f3e28a3f8e06
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-10-02 08:47:06 -0300

    Adicionando o mapa com Leaftlet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+export default async function Home() {
+  const Map = useMemo(() => dynamic(
+    () => import("@/components/Map"),
+    {
+      loading: () => <p>Carregando...</p>,
+      ssr: false
+    }
+  ), []);
+
   return (
     <div className="flex flex-col items-center justify-start min-h-screen">
       <h1 className="font-bold py-4">Mapa de Memórias 4° TI 2024</h1>
+
+      <div className="bg-white-700 mx-auto my-5 w-[98%] h-[480px]">
+        <Map posix={[-29.455639, -51.293144]} />
+      </div>
     </div>
   );
 }
