097812f255043632a51b0d0dd1b9a7e909ae51ca
unknown <aluno@D4-PC09.campus.labs>
2024-10-14 17:01:35 -0300

    added request patters to the application

---
+++ src/utils/requests/Enum.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+let spotTypes, classes;
+
+export let route = process.env.NODE_ENV === "development" ? "http://localhost:5002/api" : "https://mapamemorias.herokuapp.com"
+
+export async function getSpotTypes() {
+    return axios.get(`${route}/Enum/getSpotTypes`).then((response) => {
+        spotTypes = response.data
+    })
+}
+
+export async function getClasses() {
+    return axios.get(`${route}/Enum/getClasses`).then((response) => {
+        classes = response.data
+    })
+}
