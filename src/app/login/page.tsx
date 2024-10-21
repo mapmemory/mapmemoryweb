a2f598d03453a6a847f94246043a1a30f6a66ffb
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-10-20 23:27:22 -0300

    Getting login page done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,116 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
+import { Box, Button, TextField, ThemeProvider, Typography } from "@mui/material";
+import { useState } from "react";
+
+import { theme } from "@/components/PageBottomNavigation";
+import goBack from "@/img/goback.svg";
+
 export default function Login() {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.push("/app");
+  }
+
+  const handleGoRegister = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    router.push("/register");
+  }
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<null | string>(null);
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Complete os campos, por favor.");
+      return;
+    }
+    setError(null);
+    console.log("Login:", { email, password });
+  };
+
   return (
-    <div>Login</div>
+    <div>
+      <button
+        className="py-4 px-2"
+        onClick={handleGoBack}
+      >
+        <Image
+          src={goBack}
+          alt="Voltar"
+          width={53}
+          priority={true}
+          quality={100}
+          title="Voltar"
+        />
+      </button>
+
+      <ThemeProvider theme={theme}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            padding: 2,
+            maxWidth: 400,
+            margin: "0 auto",
+          }}
+        >
+          <h1 className="text-5xl font-black text-[#554FFF] py-3">
+            Login
+          </h1>
+
+          {error && (
+            <Typography color="error">
+              {error}
+            </Typography>
+          )}
+
+          <TextField
+            label="Email"
+            variant="outlined"
+            type="email"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <TextField
+            label="Senha"
+            variant="outlined"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Entrar
+          </Button>
+
+          <div className="text-[#554FFF] font-normal">
+            Não possui conta? 
+            <button
+              className="font-bold underline pl-2"
+              onClick={handleGoRegister}
+            >
+              Cadastre-se
+            </button>
+          </div>
+        </Box>
+      </ThemeProvider>
+    </div>
   );
 }
--- src/components/PageBottomNavigation.tsx
+++ src/components/PageBottomNavigation.tsx
@@ -2,43 +2,90 @@
 
 import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
 import { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import NavigationIcon from '@mui/icons-material/Navigation';
+import DiceIcon from '@mui/icons-material/Casino';
+import NotLoggedIcon from '@mui/icons-material/Person';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+
+import { useRouter } from "next/navigation";
+
+export const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          color: '#fff',
+          backgroundColor: '#554FFF',
+          textTransform: 'none',
+          fontWeight: 'bold',
+        }
+      }
+    },
+    MuiBottomNavigationAction: {
+      styleOverrides: {
+        root: {
+          color: '#fff',
+          '&.Mui-selected': {
+            color: '#FFD799',
+          },
+        },
+      },
+    },
+  },
+});
 
 export default function PageBottomNavigation() {
-  const [value, setValue] = useState(0);
+  const router = useRouter();
+
+  const [page, setPage] = useState(null);
+
+  const handleLoginClick = () => {
+    router.push("/login");
+  }
 
   return (
-    <Box sx={
-        {
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
           position: "fixed",
           bottom: 0,
           left: 0,
           right: 0,
-          zIndex: 100,
-        }
-      }
-    >
-      <BottomNavigation
-        sx={
-          {
-            backgroundColor: "#554FFF",
-            color: "#fff"
-          }
-        }
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-          console.log(newValue);
+          zIndex: 15,
         }}
       >
-        <BottomNavigationAction
-          color="white"
-          label="Início"
-          icon={ <NavigationIcon /> }
-        />
-      </BottomNavigation>
-    </Box>
+        <BottomNavigation
+          sx={{
+            backgroundColor: "#554FFF",
+            color: "#fff"
+          }}
+          showLabels
+          value={page}
+          onChange={(event, newPage) => {
+            setPage(newPage);
+            if (newPage === 2) {
+              handleLoginClick();
+            }
+            console.log(page);
+          }}
+        >
+          <BottomNavigationAction
+            label="Memories"
+            icon={<WidgetsIcon />}
+          />
+
+          <BottomNavigationAction
+            label="Random"
+            icon={<DiceIcon />}
+          />
+
+          <BottomNavigationAction
+            label="Login"
+            icon={<NotLoggedIcon />}
+          />
+        </BottomNavigation>
+      </Box>
+    </ThemeProvider>
   );
 }
