9bf61ba6eb78d7585de7ad453eb558a9dde56bc1
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-10-27 13:52:45 -0300

    memories visualization

--- src/app/mem/[uuid]/page.tsx
+++ src/app/mem/[uuid]/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { theme } from "@/components/PageBottomNavigation";
+import PageBottomNavigation, { theme } from "@/components/PageBottomNavigation";
 import { Box, ThemeProvider } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import goBack from "@/img/goback.svg";
+import { useEffect, useState } from "react";
+import { getMapSpotByGuid, MapSpot } from "@/utils/requests/MapSpot";
 
-import { Carousel } from "react-responsive-carousel";
+import happyImg from "@/img/emoji-happy.svg";
+import sadImg from "@/img/emoji-sad.svg";
+import angryImg from "@/img/emoji-angry.svg";
 
 export default function MemoryPage({ params }: { params: { uuid: string } }) {
   const router = useRouter();
@@ -16,20 +20,37 @@ export default function MemoryPage({ params }: { params: { uuid: string } }) {
     router.push("/app");
   }
 
+  const [marker, setMarker] = useState<MapSpot | null>(null);
+  const [isHappy, setIsHappy] = useState<boolean>(false);
+  const [isSad, setIsSad] = useState<boolean>(false);
+  const [isAngry, setIsAngry] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchMarkers = async () => {
+      try {
+        const response = await getMapSpotByGuid(params.uuid);
+        setMarker(response);
+        if (response?.type === 0) {
+          setIsHappy(true);
+        } else if (response?.type === 1) {
+          setIsSad(true);
+        } else if (response?.type === 2) {
+          setIsAngry(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (marker === null) fetchMarkers();
+  });
+
   return (
     <div>
       <button
         className="py-4 px-2"
         onClick={handleGoBack}
       >
-        <Image
-          src={goBack}
-          alt="Voltar"
-          width={53}
-          priority={true}
-          quality={100}
-          title="Voltar"
-        />
+        <Image src={goBack} alt="Voltar" width={53} priority={true} quality={100} title="Voltar" />
       </button>
 
       <ThemeProvider theme={theme}>
@@ -39,51 +60,38 @@ export default function MemoryPage({ params }: { params: { uuid: string } }) {
             flexDirection: "column",
             gap: 2,
             padding: 2,
-            maxWidth: 400,
+            maxWidth: 380,
             margin: "0 auto",
           }}
         >
-          <h1 className="text-3xl font-black text-[#554FFF] py-3">
-            Festividades coloquiais
-          </h1>
+          <div className="flex items-center text-3xl font-black text-[#554FFF] py-3">
+            {isHappy ? <Image className="inline pr-2" src={happyImg} alt="Humor" width={43} priority={true} quality={65} /> : ""}
+            {isSad ? <Image className="inline pr-2" src={sadImg} alt="Humor" width={43} priority={true} quality={65} /> : ""}
+            {isAngry ? <Image className="inline pr-2" src={angryImg} alt="Humor" width={43} priority={true} quality={65} /> : ""}
+            <h1>Memória de <span className="underline px-1">{`${marker?.name}`}</span></h1>
+          </div>
           {/* https://luisdef.com/mm/img/1.jpg */}
 
-          <Carousel showThumbs={false} infiniteLoop autoPlay transitionTime={1000}>
-            <div>
+          <div className="">
+            {marker?.picture ? 
               <Image
-                src={"https://luisdef.com/mm/img/1.jpg"}
+                className="rounded-md border-[#554FFF] border-1 shadow-xl"
+                src={`http://172.27.218.2/images/${marker.picture}`}
                 alt="Imagem"
-                width={200}
-                height={80}
+                width={380}
+                height={100}
                 priority={true}
                 quality={100}
               />
-            </div>
-            <div>
-              <Image
-                src={"https://luisdef.com/mm/img/2.jpg"}
-                alt="Imagem"
-                width={200}
-                height={80}
-                priority={true}
-                quality={100}
-              />
-            </div>
-            <div>
-              <Image
-                src={"https://luisdef.com/mm/img/3.jpg"}
-                alt="Imagem"
-                width={200}
-                height={80}
-                priority={true}
-                quality={100}
-              />
-            </div>
-          </Carousel>
-
-          <p>{params.uuid}</p>
+              : ""
+            }
+          </div>
+            
+          <p className="border-l-[2px] border-l-[#282594] bg-slate-200 p-4 rounded-e-xl  pl-2 text-[#282594] font-bold">{marker?.description}</p>
         </Box>
       </ThemeProvider>
+      
+      <PageBottomNavigation />
     </div>
   );
 }
